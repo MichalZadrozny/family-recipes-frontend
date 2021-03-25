d97316a0cfef59bee34ad37cf5d47534f11d889f
Michał Zadrożny <mt.zadrozny@gmail.com>
2021-03-25 18:37:56 +0100

    Add dynamic steps to form Additionally, the component was changed to functional component

--- src/components/AddRecipeForm/AddRecipeForm.js
+++ src/components/AddRecipeForm/AddRecipeForm.js
@@ -9,260 +9,280 @@ import * as Yup from 'yup';
 import styles from './AddRecipeForm.module.scss';
 import NutrientsFormPart from './NutrientsFormPart/NutrientsFormPart';
 
-class AddRecipeForm extends React.Component {
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, '*Nazwa musi się składać co najmniej z dwóch znaków')
+    .max(60, '*Nazwa nie może składać się z więcej niż 60 znaków')
+    .required('*Przepis musi posiadać nazwę'),
+  description: Yup.string(),
+  ingredients: Yup.array().of(
+    Yup.object().shape({
+      name: Yup.string().required('*Nazwa jest wymagana'),
+      amount: Yup.number().required('*Ilość jest wymagana').typeError('*Ilość musi być podana za pomocą liczby'),
+      unit: Yup.string(),
+    }),
+  ),
+  nutrients: Yup.object().shape({
+    calories: Yup.number().typeError('*Kalorie muszą być podane za pomocą liczby').positive('*Kalorie nie mogą być ujemne'),
+    proteins: Yup.number().typeError('*Białka muszą być podane za pomocą liczby').positive('*Białka nie mogą być ujemne'),
+    carbs: Yup.number().typeError('*Węglowodany muszą być podane za pomocą liczby').positive('*Węglowodany nie mogą być ujemne'),
+    fats: Yup.number().typeError('*Tłuszcze muszą być podane za pomocą liczby').positive('*Tłuszcze nie mogą być ujemne'),
+  }),
 
-  // eslint-disable-next-line react/state-in-constructor
-  state = {
-    // stepCounter: 2,
-  };
+  steps: Yup.array().of(
+    Yup.string().required('*Kroki do wykonania są wymagane'),
+  ),
+  time: Yup.number()
+    .typeError('*Czas musi być podany za pomocą liczby')
+    .positive('*Czas musi być większy od 0')
+    .required('*Czas jest wymagany'),
+});
 
-  render() {
-    const { addRecipe, history } = this.props;
+const form = (props) => (
+  <Form onSubmit={props.handleSubmit}>
+    <Form.Group controlId='name'>
+      <Form.Label>Nazwa przepisu</Form.Label>
+      <Form.Control
+        type='text'
+        placeholder='Nazwa przepisu'
+        name='name'
+        onChange={props.handleChange}
+        onBlur={props.handleBlur}
+        value={props.values.name}
+        className={props.touched.name && props.errors.name ? styles.error : null} />
+      {props.touched.name && props.errors.name ? (
+        <div className={styles.errorMessage}>{props.errors.name}</div>
+      ) : null}
+    </Form.Group>
+    <Form.Group controlId='description'>
+      <Form.Label>Opis</Form.Label>
+      <Form.Control
+        as='textarea'
+        rows={3}
+        placeholder='Opis'
+        name='description'
+        onChange={props.handleChange}
+        onBlur={props.handleBlur}
+        value={props.values.description}
+        className={props.touched.description && props.errors.description ? styles.error : null} />
+      {props.touched.description && props.errors.description ? (
+        <div className={styles.errorMessage}>{props.errors.description}</div>
+      ) : null}
+    </Form.Group>
+    <Form.Group>
+      <Form.Label>Składniki</Form.Label>
 
-    const initialValues = {
-      // image: '',
-      name: '',
-      description: '',
-      ingredients: [
-        {
-          name: '',
-          amount: '',
-          unit: '',
-        },
-      ],
-      diet: 'MEAT',
-      nutrients: {
-        calories: '',
-        proteins: '',
-        carbs: '',
-        fats: '',
-      },
-      steps: {
-        1: '',
-      },
-      time: '',
-    };
+      <FieldArray
+        name='ingredients'
+        render={ingredientsArray => (
+          <div>
+            {
+              props.values.ingredients.map((item, index) => {
+                const touchedIng = props.touched.ingredients;
+                const errorsIng = props.errors.ingredients;
+                return (
 
-    const validationSchema = Yup.object().shape({
-      name: Yup.string()
-        .min(2, '*Nazwa musi się składać co najmniej z dwóch znaków')
-        .max(60, '*Nazwa nie może składać się z więcej niż 60 znaków')
-        .required('*Przepis musi posiadać nazwę'),
-      description: Yup.string(),
-      ingredients: Yup.array().of(
-        Yup.object().shape({
-          name: Yup.string().required('*Nazwa jest wymagana'),
-          amount: Yup.number().required('*Ilość jest wymagana').typeError('*Ilość musi być podana za pomocą liczby'),
-          unit: Yup.string(),
-        }),
-      ),
-      nutrients: Yup.object().shape({
-        calories: Yup.number().typeError('*Kalorie muszą być podane za pomocą liczby').positive('*Kalorie nie mogą być ujemne'),
-        proteins: Yup.number().typeError('*Białka muszą być podane za pomocą liczby').positive('*Białka nie mogą być ujemne'),
-        carbs: Yup.number().typeError('*Węglowodany muszą być podane za pomocą liczby').positive('*Węglowodany nie mogą być ujemne'),
-        fats: Yup.number().typeError('*Tłuszcze muszą być podane za pomocą liczby').positive('*Tłuszcze nie mogą być ujemne'),
-      }),
+                  // eslint-disable-next-line react/no-array-index-key
+                  <div key={index}>
+                    <InputGroup>
+                      <InputGroup.Prepend>
+                        <InputGroup.Text>
+                          {index + 1}
+                        </InputGroup.Text>
+                      </InputGroup.Prepend>
 
-      // steps: Yup.string().required(),
-      time: Yup.number()
-        .typeError('*Czas musi być podany za pomocą liczby')
-        .positive('*Czas musi być większy od 0')
-        .required('*Czas jest wymagany'),
-    });
+                      <Form.Control
+                        name={`ingredients.${index}.amount`}
+                        type='number'
+                        placeholder='Ilość'
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        className={(touchedIng && touchedIng[index] && touchedIng[index].amount) && (errorsIng && errorsIng[index] && errorsIng[index].amount) ? styles.error : null}
+                      /><br />
 
-    // const stepsMap = (steps, handleChange, handleBlur, touched, errors) => Object.keys(steps).map(key =>
-    //   <StepInput key={key} index={key} steps={steps} handleChange={handleChange} handleBlur={handleBlur}
-    //              className={touched.steps && errors.steps ? styles.error : null} />,
-    // );
+                      <Form.Control
+                        name={`ingredients.${index}.unit`}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        placeholder='Jednostka'
+                      /><br />
 
-    // const addStep = (values) => {
-    //   this.setState(prevState => ({
-    //     ...prevState,
-    //     initialValues: {
-    //       ...values,
-    //       steps: {
-    //         ...values.steps,
-    //         [prevState.stepCounter]: '',
-    //       },
-    //     },
-    //   }));
-    //   this.setState(prevState => ({ stepCounter: prevState.stepCounter + 1 }));
-    // };
+                      <Form.Control
+                        name={`ingredients.${index}.name`}
+                        placeholder='Nazwa'
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        className={(touchedIng && touchedIng[index] && touchedIng[index].name) && (errorsIng && errorsIng[index] && errorsIng[index].name) ? styles.error : null}
+                      /><br />
 
-    const form = (props) => (
-      <Form onSubmit={props.handleSubmit}>
-        <Form.Group controlId='name'>
-          <Form.Label>Nazwa przepisu</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Nazwa przepisu'
-            name='name'
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-            value={props.values.name}
-            className={props.touched.name && props.errors.name ? styles.error : null} />
-          {props.touched.name && props.errors.name ? (
-            <div className={styles.errorMessage}>{props.errors.name}</div>
-          ) : null}
-        </Form.Group>
-        <Form.Group controlId='description'>
-          <Form.Label>Opis</Form.Label>
-          <Form.Control
-            as='textarea'
-            rows={3}
-            placeholder='Opis'
-            name='description'
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-            value={props.values.description}
-            className={props.touched.description && props.errors.description ? styles.error : null} />
-          {props.touched.description && props.errors.description ? (
-            <div className={styles.errorMessage}>{props.errors.description}</div>
-          ) : null}
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Składniki</Form.Label>
+                      <button type='button'
+                              onClick={() => ingredientsArray.remove(index)}> -
+                      </button>
 
-          <FieldArray
-            name='ingredients'
-            render={ingredientsArray => (
-              <div>
-                {
-                  props.values.ingredients.map((item, index) => {
-                    const touchedIng = props.touched.ingredients;
-                    const errorsIng = props.errors.ingredients;
-                    return (
+                      {(touchedIng && touchedIng[index] && touchedIng[index].amount) && (errorsIng && errorsIng[index] && errorsIng[index].amount) ? (
+                        <div className={styles.errorMessage}>{errorsIng[index].amount}</div>
+                      ) : null}
 
-                      // eslint-disable-next-line react/no-array-index-key
-                      <div key={index}>
-                        <InputGroup>
-                          <InputGroup.Prepend>
-                            <InputGroup.Text>
-                              {index + 1}
-                            </InputGroup.Text>
-                          </InputGroup.Prepend>
+                      {(touchedIng && touchedIng[index] && touchedIng[index].name) && (errorsIng && errorsIng[index] && errorsIng[index].name) ? (
+                        <div className={styles.errorMessage}>{errorsIng[index].name}</div>
+                      ) : null}
 
-                          <Form.Control
-                            name={`ingredients.${index}.amount`}
-                            type='number'
-                            placeholder='Ilość'
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            className={(touchedIng && touchedIng[index] && touchedIng[index].amount) && (errorsIng && errorsIng[index] && errorsIng[index].amount) ? styles.error : null}
-                          /><br />
+                    </InputGroup>
 
-                          <Form.Control
-                            name={`ingredients.${index}.unit`}
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            placeholder='Jednostka'
-                          /><br />
+                  </div>
 
-                          <Form.Control
-                            name={`ingredients.${index}.name`}
-                            placeholder='Nazwa'
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            className={(touchedIng && touchedIng[index] && touchedIng[index].name) && (errorsIng && errorsIng[index] && errorsIng[index].name) ? styles.error : null}
-                          /><br />
+                );
+              })}
+            <button type='button'
+                    onClick={() => ingredientsArray.push({
+                      name: '',
+                      amount: '',
+                      unit: '',
+                    })}> +
+            </button>
+          </div>
+        )}
 
-                          <button type='button'
-                                  onClick={() => ingredientsArray.remove(index)}> -
-                          </button>
-
-                          {(touchedIng && touchedIng[index] && touchedIng[index].amount) && (errorsIng && errorsIng[index] && errorsIng[index].amount) ? (
-                            <div className={styles.errorMessage}>{errorsIng[index].amount}</div>
-                          ) : null}
-
-                          {(touchedIng && touchedIng[index] && touchedIng[index].name) && (errorsIng && errorsIng[index] && errorsIng[index].name) ? (
-                            <div className={styles.errorMessage}>{errorsIng[index].name}</div>
-                          ) : null}
-
-                        </InputGroup>
-
-                      </div>
-
-                    );
-                  })}
-                <button type='button'
-                        onClick={() => ingredientsArray.push({
-                          name: '',
-                          amount: '',
-                          unit: '',
-                        })}> +
-                </button>
-              </div>
-            )}
-
-          />
-
-        </Form.Group>
-        {/* <Form.Group>
-                <Form.Label>Kroki do wykonania</Form.Label>
-                {
-                  stepsMap(values.steps, handleChange, handleBlur, touched, errors)
-                }
-                {touched.steps && errors.steps ? (
-                  <div className={styles.errorMessage}>{errors.steps}</div>
-                ) : null}
-                <Button onClick={() => addStep(values)}>Dodaj kolejny krok</Button>
-              </Form.Group> */}
-        <Form.Group controlId='preparationTime'>
-          <Form.Label>Czas przygotowania</Form.Label>
-          <Form.Control
-            type='number'
-            placeholder='Czas przygotowania'
-            name='time'
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-            value={props.values.time}
-            className={props.touched.time && props.errors.time ? styles.error : null} />
-          {props.touched.time && props.errors.time ? (
-            <div className={styles.errorMessage}>{props.errors.time}</div>
-          ) : null}
-        </Form.Group>
-        <Form.Group controlId='diet'>
-          <Form.Label>Dieta</Form.Label>
-          <Form.Control
-            as='select'
-            name='diet'
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-            value={props.values.diet}>
-            <option value='MEAT'>Mięsna</option>
-            <option value='VEGETARIAN'>Wegetariańska</option>
-            <option value='VEGAN'>Wegańska</option>
-          </Form.Control>
-        </Form.Group>
-
-        <NutrientsFormPart values={props.values} errors={props.errors} touched={props.touched}
-                           handleChange={props.handleChange}
-                           handleBlur={props.handleBlur} />
-
-        <Row className={styles.submit}>
-          <Button variant='primary' type='submit' disabled={props.isSubmitting}>
-            Submit
-          </Button>
-        </Row>
-      </Form>
-    );
-
-    return (
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        render={form}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setSubmitting(true);
-          addRecipe(values);
-          setSubmitting(false);
-          resetForm();
-          history.push('/');
-        }}
       />
-    );
-  }
-}
+
+    </Form.Group>
+
+
+    <Form.Group>
+      <Form.Label>Kroki do wykonania</Form.Label>
+
+      <FieldArray
+        name='steps'
+        render={stepsArray => (
+          <div>
+            {
+              props.values.steps.map((item, index) => {
+                const touchedSteps = props.touched.steps;
+                const errorsSteps = props.errors.steps;
+                return (
+
+                  // eslint-disable-next-line react/no-array-index-key
+                  <div key={index}>
+                    <InputGroup>
+                      <InputGroup.Prepend>
+                        <InputGroup.Text>
+                          {index + 1}
+                        </InputGroup.Text>
+                      </InputGroup.Prepend>
+
+                      <Form.Control
+                        name={`steps.${index}`}
+                        type='text'
+                        placeholder={`${index + 1} składnik`}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        className={(touchedSteps && touchedSteps[index]) && (errorsSteps && errorsSteps[index]) ? styles.error : null}
+                      /><br />
+
+                      <button type='button'
+                              onClick={() => stepsArray.remove(index)}> -
+                      </button>
+
+                      {(touchedSteps && touchedSteps[index]) && (errorsSteps && errorsSteps[index]) ? (
+                        <div className={styles.errorMessage}>{errorsSteps[index]}</div>
+                      ) : null}
+
+                    </InputGroup>
+
+                  </div>
+
+                );
+              })}
+            <button type='button'
+                    onClick={() =>
+                      stepsArray.push(
+                        '',
+                      )
+                    }> +
+            </button>
+          </div>
+        )}
+      />
+    </Form.Group>
+    <Form.Group controlId='preparationTime'>
+      <Form.Label>Czas przygotowania</Form.Label>
+      <Form.Control
+        type='number'
+        placeholder='Czas przygotowania'
+        name='time'
+        onChange={props.handleChange}
+        onBlur={props.handleBlur}
+        value={props.values.time}
+        className={props.touched.time && props.errors.time ? styles.error : null} />
+      {props.touched.time && props.errors.time ? (
+        <div className={styles.errorMessage}>{props.errors.time}</div>
+      ) : null}
+    </Form.Group>
+    <Form.Group controlId='diet'>
+      <Form.Label>Dieta</Form.Label>
+      <Form.Control
+        as='select'
+        name='diet'
+        onChange={props.handleChange}
+        onBlur={props.handleBlur}
+        value={props.values.diet}>
+        <option value='MEAT'>Mięsna</option>
+        <option value='VEGETARIAN'>Wegetariańska</option>
+        <option value='VEGAN'>Wegańska</option>
+      </Form.Control>
+    </Form.Group>
+
+    <NutrientsFormPart values={props.values} errors={props.errors} touched={props.touched}
+                       handleChange={props.handleChange}
+                       handleBlur={props.handleBlur} />
+
+    <Row className={styles.submit}>
+      <Button variant='primary' type='submit' disabled={props.isSubmitting}>
+        Submit
+      </Button>
+    </Row>
+  </Form>
+);
+
+const initialValues = {
+  // image: '',
+  name: '',
+  description: '',
+  ingredients: [
+    {
+      name: '',
+      amount: '',
+      unit: '',
+    },
+  ],
+  diet: 'MEAT',
+  nutrients: {
+    calories: '',
+    proteins: '',
+    carbs: '',
+    fats: '',
+  },
+  steps: [
+    '',
+  ],
+  time: '',
+};
+
+const AddRecipeForm = ({ addRecipe, history }) => (
+  <Formik
+    initialValues={initialValues}
+    validationSchema={validationSchema}
+    render={form}
+    onSubmit={(values, { setSubmitting, resetForm }) => {
+      setSubmitting(true);
+      addRecipe(values);
+      setSubmitting(false);
+      resetForm();
+      history.push('/');
+    }}
+  />
+);
+
 
 export default withRouter(AddRecipeForm);
 
