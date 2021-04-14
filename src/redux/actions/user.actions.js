5e450027cd7ecb3e5475c1a4114f565426de03ea
Michał Zadrożny <mt.zadrozny@gmail.com>
2021-04-14 22:16:26 +0200

    Remove unused code

--- src/redux/actions/user.actions.js
+++ src/redux/actions/user.actions.js
@@ -69,62 +69,8 @@ const register = (username, password, confirmPassword, email, termsOfUse) => {
   };
 };
 
-function getAll() {
-  function request() {
-    return { type: userConstants.GETALL_REQUEST };
-  }
-
-  function success(users) {
-    return { type: userConstants.GETALL_SUCCESS, users };
-  }
-
-  function failure(error) {
-    return { type: userConstants.GETALL_FAILURE, error };
-  }
-
-  return dispatch => {
-    dispatch(request());
-
-    userService.getAll()
-      .then(
-        users => dispatch(success(users)),
-        error => dispatch(failure(error.toString())),
-      );
-  };
-}
-
-// prefixed function name with underscore because delete is a reserved word in javascript
-// eslint-disable-next-line no-underscore-dangle
-function _delete(id) {
-  // eslint-disable-next-line no-shadow
-  function request(id) {
-    return { type: userConstants.DELETE_REQUEST, id };
-  }
-
-  // eslint-disable-next-line no-shadow
-  function success(id) {
-    return { type: userConstants.DELETE_SUCCESS, id };
-  }
-
-  // eslint-disable-next-line no-shadow
-  function failure(id, error) {
-    return { type: userConstants.DELETE_FAILURE, id, error };
-  }
-
-  return dispatch => {
-    dispatch(request(id));
-
-    userService.delete(id)
-      .then(() => dispatch(success(id)),
-        error => dispatch(failure(id, error.toString())),
-      );
-  };
-}
-
 export default withRouter({
   login,
   logout,
   register,
-  getAll,
-  delete: _delete,
 });
