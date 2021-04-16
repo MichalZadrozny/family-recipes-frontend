c48aff4076708c178e5851d0624c9dd648d69302
Michał Zadrożny <mt.zadrozny@gmail.com>
2021-04-16 13:26:14 +0200

    Add colors and tooltip for rating

--- src/components/RecipePreviewWrapper/RecipePreview/RecipeRating/RecipeRating.js
+++ src/components/RecipePreviewWrapper/RecipePreview/RecipeRating/RecipeRating.js
@@ -3,17 +3,31 @@ import PropTypes from 'prop-types';
 import styles from 'components/RecipePreviewWrapper/RecipePreview/RecipeRating/RecipeRating.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 const RecipeRating = ({ averageRating }) => (
   <div className={styles.rating}>
-    <FontAwesomeIcon icon={faStar} className={styles.ratingIcon} />
-    <span>{averageRating}</span>
+    {
+      averageRating > 0 ?
+        <>
+          <FontAwesomeIcon icon={faStar} className={styles.ratingIcon} />
+          <span>{averageRating}</span>
+        </>
+        :
+        <>
+          <OverlayTrigger overlay={<Tooltip id='tooltip'>Brak ocen</Tooltip>}>
+            <div>
+              <FontAwesomeIcon icon={faStar} className={styles.ratingIconDisabled} />
+              <span className={styles.ratingDisabled}>{averageRating}</span>
+            </div>
+          </OverlayTrigger>
+        </>
+    }
   </div>
 );
 
-
 RecipeRating.propTypes = {
   averageRating: PropTypes.number.isRequired,
-}
+};
 
 export default RecipeRating;
