06e979841c84d3166bc4203ef0c0026a9bc7a3bf
Michał Zadrożny <mt.zadrozny@gmail.com>
2021-05-02 00:09:34 +0200

    Change RecipePreview font

--- src/theme/GolbalStyle.js
+++ src/theme/GolbalStyle.js
@@ -1,33 +1,35 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
 
-*,
-*::before,
-*::after {
-	box-sizing: border-box;
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-}
+  }
 
-html{
+  html {
     font-size: 62.5%;
-}
+  }
 
-body{
-  margin: 0;
-  padding: 0;
-  background-color: #faf9f9;
-  font-size: 1.6rem;
-  font-family: "Roboto", sans-serif;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #faf9f9;
+    font-size: 1.6rem;
 
-#root {
-  display: flex;
-  flex-direction: row;
-  height: 100vh;
-}
+    font-family: 'Josefin Sans', sans-serif;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+  }
 
 `;
 export default GlobalStyle;
