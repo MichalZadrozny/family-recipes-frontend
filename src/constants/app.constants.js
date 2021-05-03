c2af2c5e6465bc9eac80c738935ee0e9472b2e93
Michał Zadrożny <mt.zadrozny@gmail.com>
2021-05-03 21:19:33 +0200

    Change backend url

--- src/constants/app.constants.js
+++ src/constants/app.constants.js
@@ -1,5 +1,4 @@
 export default {
-  // BACKEND_URL: 'https://family-recipes-dev.herokuapp.com',
-  BACKEND_URL: 'http://localhost:8080',
+  BACKEND_URL: 'https://family-recipes-dev.herokuapp.com',
   MEDIA_URL: 'https://family-recipes-bucket-dev.s3.eu-central-1.amazonaws.com/',
 };
